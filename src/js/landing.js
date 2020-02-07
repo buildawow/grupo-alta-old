4ea3cb3a82f5ca082be885a162fb09c96c41b9fa
ArielSalvadorDev <arielsalvadormejia@gmail.com>
2020-02-07 16:57:11 -0600

    wip

--- src/js/landing.js
+++ src/js/landing.js
@@ -2,12 +2,12 @@ new Vue({
     el: '#app',
     data: {
         companies: [
-            {name: 'Casa Palopo', image: './src/images/landing/casapalopo_grid@2x.jpg', logo: './src/images/logos/logo_casapalopo_white.svg'},
-            {name: 'La Esquina',  image: './src/images/landing/laesquina_grid@2x.jpg',  logo: './src/images/logos/logo_laesquina_white.svg'},
-            {name: 'Adela',       image: './src/images/landing/adela_grid@2x.jpg',      logo: './src/images/logos/logo_adela_white.svg'},
-            {name: 'Bizu',        image: './src/images/landing/bizu_grid@2x.jpg',       logo: './src/images/logos/logo_bizu_white.svg'},
-            {name: 'Queseso',     image: './src/images/landing/queseso_grid@2x.jpg',    logo: './src/images/logos/logo_queseso_white.svg'},
-            {name: 'SBT',         image: './src/images/landing/sbt_grid@2x.jpg',        logo: './src/images/logos/logo_sbt_white.svg'},
+            {name: 'Casa Palopo', image: './src/images/landing/casapalopo_grid@2x.jpg', logo: './src/images/logos/logo_casapalopo_white.svg', url: 'https://www.casapalopo.com/es/habitaciones'},
+            {name: 'La Esquina',  image: './src/images/landing/laesquina_grid@2x.jpg',  logo: './src/images/logos/logo_laesquina_white.svg', url: 'https://www.mercaditolaesquina.com/'},
+            {name: 'Adela',       image: './src/images/landing/adela_grid@2x.jpg',      logo: './src/images/logos/logo_adela_white.svg', url: 'https://www.heladosadela.com/'},
+            {name: 'Bizu',        image: './src/images/landing/bizu_grid@2x.jpg',       logo: './src/images/logos/logo_bizu_white.svg', url: 'https://shopbizu.com/'},
+            {name: 'Queseso',     image: './src/images/landing/queseso_grid@2x.jpg',    logo: './src/images/logos/logo_queseso_white.svg', url: 'https://www.facebook.com/quesesogt/'},
+            {name: 'SBT',         image: './src/images/landing/sbt_grid@2x.jpg',        logo: './src/images/logos/logo_sbt_white.svg', url: '#'},
         ],
         publications: [
             {
